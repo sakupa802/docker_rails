dceee96396a31c2642cb987c8106710be28eddb6
25312223m@gmail.com <38z25312223m@ezweb.ne.jp>
2018-03-28 18:43:35 +0900

    Revert "commit"
    This reverts commit 99658a2b98655ffaa259da5da66d39449f4dbbc7.

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  has_secure_password validations: true
-
-  validates :mail, presence: true, uniqueness: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'chat/show'
-
   # getの定義。idパラメーター付ける
   get '/home', to: 'home#index'
-  
-  # getの定義。idパラメーター付ける
-  get '/', to: 'user#index'
 end
