9990332d29904ac2a74679e477003202622a1cfb
25312223m@gmail.com <38z25312223m@ezweb.ne.jp>
2018-03-31 01:09:09 +0900

    [add] mysql追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @msg = 'こんにちは。お久しぶりです。';
+  	@users = User.where(id: 1)
+  end
+
+  def data
+  	user = User.new(name: "田中", mail: "tanaka@test.co.jp")
+  	user.save
+  	@users = User.all
+  	render :action => "index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # getの定義。idパラメーター付ける
   get '/home', to: 'home#index'
+  # getの定義。idパラメーター付ける
+  get '/data', to: 'home#data'
 end
